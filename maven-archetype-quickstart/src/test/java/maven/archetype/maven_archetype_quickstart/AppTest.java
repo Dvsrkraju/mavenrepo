41e7c638206859de7e42fcf66cf6fbf8c6f3fa25
vsrkr <vsrkr@Siva_Dandu>
2024-03-21 20:25:19 +0530

    new change

---
+++ maven-archetype-quickstart/src/test/java/maven/archetype/maven_archetype_quickstart/AppTest.java
@@ -0,0 +1,20 @@
+package maven.archetype.maven_archetype_quickstart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
